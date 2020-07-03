5a0cbd665f4fb1e450dea2f083c62c5c534d90b0
Teja <phankanmp@gmail.com>
2020-07-02 22:58:34 -0700

    change test to implement new hashing function

--- backend/tests/util/test_generate_hash.py
+++ backend/tests/util/test_generate_hash.py
@@ -3,5 +3,6 @@ from backend.blockchain.Block import Block
 
 
 def test_generate_hash():
-    assert generate_hash(1, '2', [3]) == generate_hash(1, [3], '2')
-    assert Block.generate_genesis().hash == '3d676e303d254e846fe8196df400983037e32458bf5d08a77609836a9b3b2869'
+    block = Block.generate_genesis()
+    assert generate_hash(block) == generate_hash(block)
+    assert generate_hash(block) == 'a65ae033a322f80df233c856ae2cbb2677b16a69eebeda4c95b3cea25a86092d'
