0400934a4f1b0c099870ef4d92cccd29f57e08dd
Teja <phankanmp@gmail.com>
2020-07-05 23:40:19 -0700

    add calculate balance implementation for wallets

--- backend/wallet/wallet.py
+++ backend/wallet/wallet.py
@@ -36,3 +36,36 @@ class Wallet:
             ec.ECDSA(hashes.SHA256())
         )
 
+    def calculate_balance(self, blockchain):
+        balance = self.balance
+
+        transactions = []
+        wallet_transactions = []
+
+        for block in blockchain.chain:
+            for transaction in block.data:
+                transactions.append(transaction)
+
+        for transaction in transactions:
+            if transaction.input['address'] == self.public_key:
+                wallet_transactions.append(transaction)
+
+        start_time = 0
+
+        if len(wallet_transactions) > 0:
+            latest_transaction = wallet_transactions[0].input['timestamp']
+
+            for wallet_transaction in wallet_transactions:
+                if wallet_transaction.input['timestamp'] > latest_transaction:
+                    latest_transaction = wallet_transaction.input['timestamp']
+
+            balance = latest_transaction.outputs['sender_amount']
+
+            start_time = latest_transaction.input['timestamp']
+
+        for transaction in transactions:
+            if transaction.input['timestamp'] > start_time:
+                if transaction.outputs['recipient_address'] == self.public_key:
+                    balance += transaction.outputs['recipient_amount']
+
+        return balance
