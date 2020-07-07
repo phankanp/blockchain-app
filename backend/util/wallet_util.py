b930dfd052944960b2a8c0ab47630938957895cf
Teja <phankanmp@gmail.com>
2020-07-06 18:32:26 -0700

    change generate and verify signature implementations

--- backend/util/wallet_util.py
+++ backend/util/wallet_util.py
@@ -5,6 +5,10 @@ from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes, serialization
 from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.utils import (
+    encode_dss_signature,
+    decode_dss_signature
+)
 
 
 class WalletUtil:
@@ -49,13 +53,23 @@ class WalletUtil:
         return str(uuid.uuid4())
 
     @staticmethod
+    def sign(key_pair, data):
+        """
+        Generates transaction signature
+        """
+        return decode_dss_signature(key_pair.sign(
+            json.dumps(data).encode('utf-8'),
+            ec.ECDSA(hashes.SHA256())
+        ))
+
+    @staticmethod
     def verify_signature(public_key, signature, data):
         """
         Verifies transaction signature
         """
         try:
             public_key.verify(
-                signature,
+                encode_dss_signature(signature),
                 json.dumps(data).encode('utf-8'),
                 ec.ECDSA(hashes.SHA256())
             )
