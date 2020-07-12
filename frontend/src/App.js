e3bc53d7a1fcf36a832026e83b3c2fbb4e4438d5
Teja <phankanmp@gmail.com>
2020-07-12 13:46:32 -0700

    add initial frontend files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Button } from 'react-bulma-components';
+
+
+function App() {
+  return (
+    <div className="App has-text-centered">App
+    </div>
+  );
+}
+
+export default App;
